8220fcd6231035456e09d669141fd794beda1a3c
tatyanka03 <63300782+tatyanka03@users.noreply.github.com>
2020-05-26 08:31:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace Class_and_constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            person p = new person("Пупкин", "Вася");
+
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.SekondName);
+
+            Console.ReadLine();
+        } 
+    }
+}

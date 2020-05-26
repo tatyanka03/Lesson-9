90330475a75a032e9c88feaafce1bf75acb9ea9d
tatyanka03 <63300782+tatyanka03@users.noreply.github.com>
2020-05-26 08:28:56 +0300

    Add files via upload

---
+++ person(9 lesson).cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_and_constructor
+{
+    public class person
+    {
+        public string SekondName { get; set; }
+
+        public string Name { get; set; }
+        
+        
+        public person(string sekondName, string name)
+        {
+           
+            SekondName = sekondName;
+            Name = name;
+        }
+        
+        
+          
+        
+    }
+
+}
